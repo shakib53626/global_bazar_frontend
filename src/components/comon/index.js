3a5a089bc39b36e8e9233d1e53c6e3090239ae65
shakib53626 <diu.shakib@gmail.com>
2023-11-07 03:58:14 +0600

    product Component add

--- src/components/comon/index.js
+++ src/components/comon/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from '@/components/comon/Header.vue';
 export { default as Footer } from '@/components/comon/Footer.vue';
 export { default as QuickViewModal } from '@/components/comon/QuickViewModal.vue';
+export { default as Products } from '@/components/comon/Product.vue';
---
+++ src/stores/themeOptions.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia'
+
+export const useThemeSetting = defineStore('themeSetting', {
+    state: () => ({ 
+        themesInfo: {},
+        currency:'taka',
+        language:'bangla',
+    }),
+    persist: {
+        paths: ['currency', 'language'],
+    },
+    getters: {
+
+    },
+    actions: {
+        async getThemeData(){
+            try {
+                this.themesInfo = {
+                    phone_number: '+880 178 480 1663',
+                    logo_image: './src/assets/images/menu/logo/1.jpg',
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+})
